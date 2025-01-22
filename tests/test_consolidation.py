989f77e49e9ec7f4c13139c00279ea26d6ab4704
Simon Perkins <simon.perkins@gmail.com>
2025-01-22 11:24:23 +0200

    Add test_deconsolidate_metadata

--- tests/test_consolidation.py
+++ tests/test_consolidation.py
@@ -64,3 +64,36 @@ def test_consolidated_at_dataset(populated_zarr_store, consolidated):
   # https://zarr.readthedocs.io/en/stable/user-guide/consolidated_metadata.html#usage
   for _, group in root.groups():
     assert (group.metadata.consolidated_metadata is not None) is consolidated
+
+
+
+def deconsolidate_metadata(store):
+  with open(store / "zarr.json") as f:
+    metadata_str = f.read()
+
+  import json
+
+  metadata = json.loads(metadata_str)
+  metadata["consolidated_metadata"] = None
+
+  with open(store / "zarr.json", mode="w") as f:
+    f.write(json.dumps(metadata))
+
+
+def test_deconsolidate_metadata(populated_zarr_store):
+  # Consolidating metdata at the group level works
+  zarr.consolidate_metadata(populated_zarr_store)
+  root = zarr.open_group(populated_zarr_store)
+  assert root.metadata.consolidated_metadata is not None
+  group_names = {n for n, _ in root.groups()}
+
+  # Remove in the root level "zarr.json"
+  deconsolidate_metadata(populated_zarr_store)
+
+  root = zarr.open_group(populated_zarr_store)
+  assert root.metadata.consolidated_metadata is None
+  assert group_names == {n for n, _ in root.groups()}
+
+  for _, g in root.groups():
+    assert "DATA" in g
+    assert "INDEX" in g
